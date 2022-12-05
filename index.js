d8c0e3cae608e36fa1986d91d1197c8227bfee80
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-05 20:16:41 +0100

    Publish version 1.0.1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const Node = require('./graph/Node');
+const ChessVertex = require('./graph/chess/ChessVertex');
 const ChessGraph = require('./graph/chess/ChessGraph');
 const KnightGraph = require('./graph/chess/KnightGraph');
 
 module.exports = {
+  Node,
+  ChessVertex,
   ChessGraph,
   KnightGraph,
 };
